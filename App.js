906a6dd1f1876a8f8192ce6b81755d9543db9a5c
Angel Aranibar <angelaranibar@MacBook-Pro-de-Angel.local>
2020-06-14 03:11:19 -0400

    Onboarding

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,  Text, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  import imagenes from "./src/Config/Imagenes"
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  import Onboarding from "./src/Pantallas/OnboardingFactory"
+
+
+  function Ejemplo({navigation}) {
+    return (
+      <View>
+        <Text style={{textAlign: 'center', marginTop: 300}}>
+          Este es un ejemplo de una pantalla final que se coloque despues del Onboarding.
+          Tambien se puede añadir si se desea otra pila de navegacion.
+        </Text>
+      </View>
+    )
+  }
+
+const App: () => React$Node = () => {
+  return <Onboarding
+    pantallas = {[
+      {
+        color : "#f00",
+        Titulo : "Titulo Uno",
+        Descripcion : "Descripcion Uno",
+        imagen : imagenes.img1
+      },
+      {
+        color : "#0f0",
+        Titulo : "Titulo Dos",
+        Descripcion : "Descripcion Dos",
+        imagen : imagenes.img2
+      },
+      {
+        color : "#00f",
+        Titulo : "Titulo Tres",
+        Descripcion : "Descripcion Tres",
+        imagen : imagenes.img3
+      }
+    ]}
+    Ultimo = {Ejemplo}
+    />;
+};
+
+export default App;
